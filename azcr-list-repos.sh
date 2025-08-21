a67e94a61c785c6b4b3ae5147ae434e9dda3445f
ihsanozlu <ihsanozlu@gmail.com>
2025-08-21 10:55:23 +0300

    updated repositoryname

--- azcr-list-repos.sh
+++ azcr-list-repos.sh
@@ -1,5 +1,5 @@
 
-az acr repository list --name azerlottery --output tsv | wc -l
+az acr repository list --name repositoryname --output tsv | wc -l
 184 repository
 Total Images: 20053
 Totat Size 4.1 TB
@@ -7,8 +7,8 @@ Totat Size 4.1 TB
 ----
 
 total_images=0
-az acr repository list --name azerlottery --output tsv | while read repo; do
-  count=$(az acr manifest list-metadata -r azerlottery -n $repo -o tsv | wc -l)
+az acr repository list --name repositoryname --output tsv | while read repo; do
+  count=$(az acr manifest list-metadata -r repositoryname -n $repo -o tsv | wc -l)
   total_images=$((total_images + count))
   echo "Total Images: $total_images"
 done
@@ -18,11 +18,11 @@ echo "Total Images: $total_images"
 ----
 
 total_size=0
-az acr repository list --name azerlottery --output tsv | while read repo; do
-  latest_tag=$(az acr manifest list-metadata -r azerlottery -n $repo --query "[?tags[?=='latest']].tags[0]" --output tsv)
+az acr repository list --name repositoryname --output tsv | while read repo; do
+  latest_tag=$(az acr manifest list-metadata -r repositoryname -n $repo --query "[?tags[?=='latest']].tags[0]" --output tsv)
   
   if [ ! -z "$latest_tag" ]; then
-    latest_size=$(az acr manifest list-metadata -r azerlottery -n $repo --query "[?tags[?=='latest']].imageSize" --output tsv)
+    latest_size=$(az acr manifest list-metadata -r repositoryname -n $repo --query "[?tags[?=='latest']].imageSize" --output tsv)
     
     total_size=$((total_size + latest_size))
     
@@ -39,11 +39,11 @@ echo "Total Size of 'latest' Images: $total_size bytes"
 
 
 total_size=0
-az acr repository list --name azerlottery --output tsv | while read repo; do
-  latest_tag=$(az acr manifest list-metadata -r azerlottery -n $repo --query "[?contains(tags, 'latest')].tags[0]" --output tsv)
+az acr repository list --name repositoryname --output tsv | while read repo; do
+  latest_tag=$(az acr manifest list-metadata -r repositoryname -n $repo --query "[?contains(tags, 'latest')].tags[0]" --output tsv)
   
   if [ ! -z "$latest_tag" ]; then
-    latest_size=$(az acr manifest list-metadata -r azerlottery -n $repo --query "[?contains(tags, 'latest')].imageSize" --output tsv)
+    latest_size=$(az acr manifest list-metadata -r repositoryname -n $repo --query "[?contains(tags, 'latest')].imageSize" --output tsv)
   
     total_size=$((total_size + latest_size))
     
@@ -60,11 +60,11 @@ echo "Total Size of 'latest' Images: $total_size bytes"
 
 
 total_size=0
-az acr repository list --name azerlottery --output tsv | while read repo; do
-  latest_tag=$(az acr manifest list-metadata -r azerlottery -n $repo --query "[?contains(tags, 'latest')].tags[0]" --output tsv)
+az acr repository list --name repositoryname --output tsv | while read repo; do
+  latest_tag=$(az acr manifest list-metadata -r repositoryname -n $repo --query "[?contains(tags, 'latest')].tags[0]" --output tsv)
   
   if [ "$latest_tag" = "latest" ]; then
-    latest_size=$(az acr manifest list-metadata -r azerlottery -n $repo --query "[?contains(tags, 'latest')].imageSize" --output tsv)
+    latest_size=$(az acr manifest list-metadata -r repositoryname -n $repo --query "[?contains(tags, 'latest')].imageSize" --output tsv)
     
     total_size=$((total_size + latest_size))
     
